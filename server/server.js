f5c9628138d4c76c285dea99779c964444088a5d
mandomat <mandom@hotmail.it>
2016-10-24 18:23:12 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 var http = require("http"),
   port = 8080;
-var sensor = require("./sensor.js"); 
+var sensor = require("./sensor.js");
 var dist = "-1";
 var movement = require("./movement.js")
- 
-
+var spawn = require('child_process').spawn;
+var fs = require('fs');
 
 
 module.exports = function() {
   var server = http.createServer(function(request, response) {
 
-  
 
-    
+
+
     if ( request.method == 'GET') {
       //MOVE FORWARD AND GREEN LED ON
-      if(request.url=='/moveFRWRD') { 
-        movement.moveFRWRD();  
+      if(request.url=='/moveFRWRD') {
+        movement.moveFRWRD();
         console.log("moving forward");
-       
+
       }
       //MOVE BACKWARD AND RED LED ON
-       else if(request.url == '/moveBCKWRD'){
-       movement.moveBCKWRD();
-       console.log("moving backward");
+      else if(request.url == '/moveBCKWRD'){
+        movement.moveBCKWRD();
+        console.log("moving backward");
 
       }
       //STOP AND RED LED ON
@@ -33,29 +33,40 @@ module.exports = function() {
       }
       //CALCULATE DISTANCE
       else if(request.url == '/distance'){
-       dist = sensor.calculate();
-       console.log(""+dist);
-       }
-       //TURN RIGHT AND LEDs OFF
-       else if (request.url == '/turnRight'){
-       movement.turnRight();
-       console.log("turning right");
-       } 
-       //TURN LEFT AND LEDs OFF
-       else if (request.url == '/turnLeft'){
-       movement.turnLeft();
-       console.log("turning left");
-       }
-
-       
-
+        dist = sensor.calculate();
+        console.log(""+dist);
+      }
+      //TURN RIGHT AND LEDs OFF
+      else if (request.url == '/turnRight'){
+        movement.turnRight();
+        console.log("turning right");
+      }
+      //TURN LEFT AND LEDs OFF
+      else if (request.url == '/turnLeft'){
+        movement.turnLeft();
+        console.log("turning left");
+      }
+      //TAKE PHOTO (ALLOW STREAMING)
+      else if(request.url == '/takePhoto') {
+        var args = ["-w", "640", "-h", "480", "-t", "100", "-o", "image.jpg"]
+        spawn('raspistill', args);
+        var image = fs.readFileSync('./image.jpg');
+      }
+        if(reques.url=='/takePhoto'){
         response.writeHeader(200, {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
-      });
-           response.write("" + dist);
-           response.end();
+          "Content-Type": "image/jpg",
+          "Access-Control-Allow-Origin": "*"
+        });
+        response.end(image,'binary');
 
+      }else {
+          response.writeHeader(200, {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+          });
+          response.write("" + dist);
+          response.end();
+        }
     } else {
       response.writeHeader(200, {
         "Content-Type": "application/json",
