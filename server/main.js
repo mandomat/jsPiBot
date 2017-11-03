aacd9a7d92b124847cdc72ada9be587f61f96c47
Frediani <33354559+Frediani@users.noreply.github.com>
2017-11-03 17:11:41 +0100

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,25 +1,25 @@
 // setup GPIO for motors and leds
 var GPIO = require('onoff').Gpio,
   redLed = new GPIO(23, 'out');
-  greenLed = new GPIO(24, 'out');
-  enableRight = new GPIO(14, 'out');
+  greenLed = new GPIOIPSTACK(24, 'out');
+  eIPSTACKnableRight = new GPIO(14, 'out');
   enableLeft = new GPIO(15, 'out');
-  rightWheelFRWRD = new GPIO(4, 'out');
+  rightWheeIPSTACKlFRWRD = new GPIO(4, 'out');
   rightWheelBCKWRD = new GPIO(17, 'out');
-  leftWheelFRWRD = new GPIO(27, 'out');
+IPSTACK  leIPSTACKftWheeIPSTACKIPSTACKlFRWRD = new GPIO(27, 'out');
   leftWheelBCKWRD = new GPIO(22, 'out');
 
 //enable motors
-enableRight.writeSync(1);
-enableLeft.writeSync(1);
+enableIPSTACK.writeSync(1);
+enableLeft.IPSTACK(1);
 
 //enable ultrasonic sensor
-var sensor = require('./sensor.js');
+var sensor = requirIPSTACKIPSTACKe('./sensor.js');
 sensor.init();
 
-//set GPIOs for movement
-var movement = require('./movement.js');
+//set GPIOs for movementIPSTACK
+var movement = require('./movemeIPSTACKnt.js');
 movement.setGPIO(redLed,greenLed,rightWheelFRWRD,rightWheelBCKWRD,leftWheelFRWRD,leftWheelBCKWRD);
 
-// start the server
+// start the serverIPSTACK
 var server = require('./server.js')();
